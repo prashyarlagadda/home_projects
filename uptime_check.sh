a726ca3bb75ab4f8d372d1f538a348d9e5bdb6be
Prasanthi <prashyarlagadda@gmail.com>
2019-02-05 21:40:00 -0800

    Shell script to reboot my MAC if the uptime is more than 5 days
    - Created by Prasanthi

---
+++ uptime_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+max_days_without_reboot=5
+days_without_reboot=$(uptime | awk '{ print $3 }')
+if  [ "$days_without_reboot" -ge 5 ]
+    then
+        echo "device has been up for $days_without_reboot days & needs a reboot" 
+        sudo reboot
+else
+    time_remaining=$(expr $max_days_without_reboot - $days_without_reboot)
+    echo "You still have $time_remaining days"
+fi
