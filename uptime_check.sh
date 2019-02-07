2a09c5f0bae3225be1b9e744888f318da22b2c44
Prasanthi <prashyarlagadda@gmail.com>
2019-02-06 21:42:52 -0800

    added colors for the output

--- uptime_check.sh
+++ uptime_check.sh
@@ -2,13 +2,23 @@
 
 #Script to re-boot the device after 5 days
 
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+SET='\033[0m'
 max_days_without_reboot=5
 days_without_reboot=$(uptime | awk '{ print $3 }')
+time_remaining=$(expr $max_days_without_reboot - $days_without_reboot)
 if  [ "$days_without_reboot" -ge 5 ]
     then
-        echo "device has been up for $days_without_reboot days & needs a reboot" 
+        echo "Device has been up for $days_without_reboot days & needs a reboot" 
         sudo reboot
+elif [ "$time_remaining" -le 2 ]
+    then
+        echo -e "${RED}You still have $time_remaining days to Reboot${SET}"
+elif [ "$time_remaining" == 3 ]
+    then
+        echo -e "${YELLOW}You still have $time_remaining days to Reboot${SET}"
 else
-    time_remaining=$(expr $max_days_without_reboot - $days_without_reboot)
-    echo "You still have $time_remaining days"
+        echo -e "${GREEN}You still have $time_remaining days to Reboot${SET}"
 fi
